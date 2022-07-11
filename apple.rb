9c0562e9718396021a3d96acf497f47e5f1b1ba5
noxtsu <kuraraiban29@gmail.com>
2022-07-11 09:47:40 +0000

    [add]apple

---
+++ apple.rb
@@ -0,0 +1,21 @@
+apple = "Nagano"
+
+if apple == "Aomori"
+  puts "このリンゴは青森県産です。"
+elsif apple == "Nagano"
+  puts "このリンゴは長野県産です。"
+else
+  puts "このリンゴは青森県産でも長野県産でもありません"
+end
+
+
+money = 60
+
+if money > 100
+  puts "みかんを購入。所持金に余りあり。"
+elsif money == 100
+  puts "みかんを購入。所持金は0円"
+else
+  puts "みかんを購入することができません。"
+end
+
